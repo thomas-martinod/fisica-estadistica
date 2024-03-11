eca03f97b7805e734e7368791089e00868ad57cb
Thomas Martinod <thom.martinod@gmail.com>
2024-03-10 21:18:54 -0500

    done

--- parcial-1/tarea-1.py
+++ parcial-1/tarea-1.py
@@ -2,22 +2,28 @@ import numpy as np
 
 # Constants
 pi = np.pi
-R = 8.314462618  # J/(mol*K) - Ideal gas constant
-T = 298.0        # K - Temperature
-P = 1.0          # atm - Pressure
+R = 8.314462618
+T = 300
+P = 1.0
 h = 6.62607015e-34
 kB = 1.380649e-23
+NA = R/kB
+# He, Ne, Al, Ba, Ar, Be, Br, C
+molar_masses = [4.00260128, 20.18004638, 26.98153841, 137.32667172, 39.94779856, 9.01218306, 79.90432616, 12.01063556]
+
+def get_mass_of_an_atom (molarmass: float):
+    return molarmass / NA / 1000  # Se retorna en kg/atomo
 
 # Translational partition function
 def translational_partition(m):
-    return ((2.0 * pi * m * kB * T) / (h**2))**(3.0 / 2.0) * (kB * T / P)
+    return ((2 * pi * m * kB * T) / (h**2))**(3/2) * (kB * T / P)
 
-# Mass (in kg/mol)
-mass = 28.97e-3  # Molar mass of nitrogen in kg/mol
+def entropia(q):
+    return 3/2 * kB * np.log(q)
 
-# Calculate the translational partition function for the given mass
-translational_partition_value = translational_partition(mass)
+mass_he = get_mass_of_an_atom(molar_masses[0])
+print(mass_he)
+tras_He = translational_partition(mass_he)
+print(tras_He)
+print(entropia(tras_He))
 
-# Print the result
-print("Molar mass:", mass, "kg/mol")
-print("Translational partition function:", translational_partition_value)

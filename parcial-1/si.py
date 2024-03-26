426b0f9e3c922d1654542e9d37376a4bcf606272
Thomas Martinod <110356084+thomas-martinod@users.noreply.github.com>
2024-03-26 01:20:12 -0500

    up

--- parcial-1/si.py
+++ parcial-1/si.py
@@ -4,61 +4,117 @@ import numpy as np
 pi = np.pi                  # Pi
 h = 6.62607015e-34          # Plank's constant (J s)
 k_B = 1.380649e-23          # Boltzmann constant (J / K)
-N_A = 6.02214076e+23        # Avogadro's Number (1 / mol)
-R = k_B * N_A               # Ideal gas constant (J / mol K)
+N_A = 6.02214076e+23        #s Avogadro's Number (1 / mol)
+
+class Tras:
+    def __init__(self, m, N, T, P) -> None:
+        self.m = m / 1000 / N_A  # En kg
+        self.N = N
+        self.T = T
+        self.P = P
+        self.V = self.N * k_B * self.T / self.P
+        self.q = (2 * pi * self.m * k_B * self.T / h**2)**(3/2) * self.V
+        self.U = 1.5 * self.N * k_B * self.T
+        self.S = self.N * k_B * (2.5 + np.log(self.q / self.N))
+        self.A = -self.N * k_B * self.T * (np.log(self.q / self.N) + 1)
+        self.G = -self.N * k_B * self.T * np.log(self.q / self.N)
+        self.H = 2.5 * self.N * k_B * self.T
+        self.mu = - k_B * self.T * np.log(self.q / self.N)
+    def get_q(self):
+        return self.q
+
+    def get_U(self):
+        return self.U
+
+    def get_S(self):
+        return self.S
+
+    def get_A(self):
+        return self.A
+
+    def get_G(self):
+        return self.G
+
+    def get_H(self):
+        return self.H
+
+    def get_mu(self):
+        return self.mu
 
 
-def mass_of_an_atom(m: float):
-    return m / N_A / 1000  # (kg / atom)
+class Tras_and_ele:
+    def __init__(self, m, N, T, P, ge1) -> None:
+        self.m = m
+        self.N = N
+        self.T = T
+        self.P = P
+        self.V = self.N * k_B * self.T / self.P
+        self.q_tras = (2 * pi * self.m * k_B * self.T / h**2)**(3/2) * self.V
+        self.q_ele = ge1
+        self.U = 1.5 * self.N * k_B * self.T
+        self.S = self.N * k_B * (2.5 + np.log(self.q_tras * self.q_ele / self.N))
+        self.A = -self.N * k_B * self.T * (np.log(self.q_tras * self.q_ele / self.N) + 1)
+        self.G = -self.N * k_B * self.T * np.log(self.q_tras * self.q_ele / self.N)
+        self.H = 2.5 * self.N * k_B * self.T
+        self.mu = - k_B * self.T * np.log(self.q_tras * self.q_ele / self.N)
 
+    def set_m(self, m):
+        self.m = m
 
-def translational_partition(m, T, V):
-    return ((2 * pi * m * k_B * T) / (h ** 2)) ** (3 / 2) * V
+    def set_N(self, N):
+        self.N = N
 
+    def set_T(self, T):
+        self.T = T
 
-def internal_energy(T):
-    return 3 / 2 * R * T
+    def set_P(self, P):
+        self.P = P
 
+    def get_q(self):
+        return self.q
 
-def entropy(q, N):
-    return N * (3 / 2 * k_B + k_B * np.log(q))
+    def get_U(self):
+        return self.U
 
+    def get_S(self):
+        return self.S
 
-def gibbs(q, N, T):
-    return N * k_B * T * (1 - np.log(q))
+    def get_A(self):
+        return self.A
 
+    def get_G(self):
+        return self.G
 
-def electronic_partition(ge):
-    return ge
+    def get_H(self):
+        return self.H
 
-
-def entropy_ele(q_ele):
-    return R * np.log(q_ele)
-
-
-def entropy_both(q_tras, q_ele):
-    return 5 / 2 * R + R * np.log(q_tras * q_ele / N_A)
-
+    def get_mu(self):
+        return self.mu
 
 def main():
-    T = 298  # Standard Temperature (K)
-    P = 10 ** 5  # Standard pressure (Pa)
-    V = k_B * T / P  # Standard volume (m^3)
-    N = N_A  # Number of particles
+    # Ingreso de data
+    aux = input('Ingrese las condiciones de presión, temperatura y número de moles separadas por comas (si ingresa \'st\' se asume 1 bar, 298K y un mol):')
+    if aux == 'st':
+        P = 10000
+        T = 298.15
+        N = N_A
+    else:
+        pass
+    elem = input('Ingrese el elemento a considerar: ')
+    mass = float(input('Ingrese su masa: '))
+    config = input('Ingrese su terminación de configuración electrónica (ej: \'3p^4\'): ')
+    # Create an instance of the tras class
+    sys_tras = Tras(mass, N, T, P)
 
-    print('Con este programa calculas U_298, S_298, G_298.')
-    atom_in = input('Ingrese el átomo: ')
-    m_in = float(input('Ingrese la masa: '))  # Convert to float
-    ge_in = float(input('Ingrese la degenerancia del estado electrónico: '))  # Convert to float
+    # Print out the values of q, U, S, A, G, H, and mu
+    print("q =", sys_tras.get_q())
+    print("U =", sys_tras.get_U())
+    print("S =", sys_tras.get_S())
+    print("A =", sys_tras.get_A())
+    print("G =", sys_tras.get_G())
+    print("H =", sys_tras.get_H())
+    print("μ =", sys_tras.get_mu())
 
-    print('--------------------------------------------------------')
-    print('Función de partición traslacional')
-    m = mass_of_an_atom(m_in)
-    q_tras = translational_partition(m, T, V)
-    print('q_tras = ' + str(q_tras))
-    print('\nU°_298 = ' + str(internal_energy(T) / 1000) + 'kJ')
-    print('S°_298 = ' + str(entropy(q_tras, N)) + 'unidad')
-    print('G°_298 = ' + str(gibbs(q_tras, N, T)))
+if __name__ == "__main__":
+    main()
 
-
-main()
